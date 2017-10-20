d43c8b936fc1054445762c9e2741c71935c3a1af
Yao Lu <luyaocns@gmail.com>
2017-10-20 09:49:06 +0800

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,11 @@ using CMAES
 
 N = 100
 x0, σ0, lo, hi = 0.3ones(N), 0.2, fill(-5.12, N), fill(5.12, N)
-xmin, fmin = CMAES.minimize(rastrigin, x0, σ0, lo, hi; maxfevals = 100000)
+xmin, fmin = CMAES.minimize(rastrigin, x0, σ0, lo, hi; maxfevals = 10000)
 
-# using CMA
-# xmin, fmin = CMA.minimize(rastrigin, x0, σ0, lo, hi; maxfevals = 10000) #, pool = [2:2:20;])
-#
-# using BlackBoxOptim
-# res = bboptimize(rastrigin; SearchRange = (-5.12, 5.12), NumDimensions = N, Method = :dxnes, MaxFuncEvals = 10000, ini_x = x0, ini_sigma = σ0)
-# xmin, fmin = best_candidate(res), best_fitness(res)
+using CMA
+xmin, fmin = CMA.minimize(rastrigin, x0, σ0, lo, hi; maxfevals = 10000)
+
+using BlackBoxOptim
+res = bboptimize(rastrigin; SearchRange = (-5.12, 5.12), NumDimensions = N, Method = :dxnes, MaxFuncEvals = 10000, ini_x = x0, ini_sigma = σ0)
+xmin, fmin = best_candidate(res), best_fitness(res)
